b3b678cc156ece3bd678fa820489bcbbaa92af08
gidoBOSSftw5731 <gidoBOSSftw5731@users.noreply.github.com>
2020-07-02 01:51:12 +0000

    Create entropytime.sh

---
+++ entropytime.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+#this has to be a large file, I use a different one but did not get clearance to post it, so I found another one that is probably big enough. If this isnt large enough you should probably invest in a hardware RNG.
+wget -o /dev/null -O /dev/null https://speed.hetzner.de/10GB.bin
